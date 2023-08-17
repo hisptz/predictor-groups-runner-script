ef6fc2d53bd0e8d370cda8e1ecc5405609506b87
megamind <echingalo@gmail.com>
2023-08-18 01:40:28 +0300

    refactor: implemented the predictor utilities and services

---
+++ src/utils/predictors.ts
@@ -0,0 +1,58 @@
+import { dhis2Client } from "../clients";
+import logger from "../logging";
+import { PredictorGroup } from "../types";
+
+export async function getPredictorGroups(): Promise<PredictorGroup[]> {
+  logger.info(`Fetching Predictors groups`);
+  const url = "predictorGroups?paging=false&fields=id,name";
+  try {
+    const response = await dhis2Client.get(url);
+
+    if (response.status !== 200) {
+      logger.error(
+        `Failed to fetch predictor groups! ${response?.data?.message ?? ""}`
+      );
+      return [];
+    }
+    const predictorGroups = (response?.data?.predictorGroups ??
+      []) as PredictorGroup[];
+    logger.info(
+      `Successfully fetched ${predictorGroups.length} predictor groups`
+    );
+    return predictorGroups;
+  } catch (error) {
+    logger.error(`Failed to fetch predictor groups! See the error bellow.`);
+    logger.error(`${JSON.stringify(error)}`);
+    return [];
+  }
+}
+
+export async function runPredictorGroup(
+  predictorGroup: PredictorGroup,
+  startDate: string,
+  endDate: string
+): Promise<void> {
+  logger.info(
+    `Generating predictions for ${predictorGroup.name} Group from ${startDate} to ${endDate}`
+  );
+  try {
+    const url = `predictorGroups/${predictorGroup.id}/run?startDate=${startDate}&endDate=${endDate}`;
+    const response = await dhis2Client.post(url);
+
+    if (response.status !== 200) {
+      logger.error(
+        `Failed to generate the predictions! ${response?.data?.message ?? ""}`
+      );
+      return;
+    }
+
+    const message =
+      response?.data?.message ?? "Successfully generated the predictions";
+    logger.info(message);
+  } catch (error) {
+    logger.error(
+      `Failed to run ${predictorGroup.name} group! See the error below.`
+    );
+    logger.error(`${JSON.stringify(error)}`);
+  }
+}
